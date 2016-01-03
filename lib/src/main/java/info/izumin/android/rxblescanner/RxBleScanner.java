24f4d623f01a296839cecb5bf1188d3641ae3888
izumin5210 <masayuki@izumin.info>
2016-01-04 00:39:43 +0900

    Use RxBleScannerL on RxBleScanner when the build version is greater than lolipop

--- lib/src/main/java/info/izumin/android/rxblescanner/RxBleScanner.java
+++ lib/src/main/java/info/izumin/android/rxblescanner/RxBleScanner.java
@@ -6,6 +6,7 @@ import android.bluetooth.le.ScanResult;
 import android.os.Build;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * Created by izumin on 1/2/2016 AD.
@@ -28,9 +29,18 @@ public class RxBleScanner {
     }
 
     public Observable<ScanResultJB> startScanJB() {
-        final RxBleScannerJB scanner = new RxBleScannerJB(adapter);
-        scannerImpl = scanner;
-        return scanner.startScan();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return startScan().map(new Func1<ScanResult, ScanResultJB>() {
+                @Override
+                public ScanResultJB call(ScanResult result) {
+                    return new ScanResultJB(result);
+                }
+            });
+        } else {
+            final RxBleScannerJB scanner = new RxBleScannerJB(adapter);
+            scannerImpl = scanner;
+            return scanner.startScan();
+        }
     }
 
     public void stopScan() {
