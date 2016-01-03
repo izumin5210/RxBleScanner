b9b9c681e11bcce2299e40bb34027ca4de9300c3
izumin5210 <masayuki@izumin.info>
2016-01-04 00:38:47 +0900

    Add constructor to ScanResultJB for ScanResult

--- lib/src/main/java/info/izumin/android/rxblescanner/ScanResultJB.java
+++ lib/src/main/java/info/izumin/android/rxblescanner/ScanResultJB.java
@@ -1,6 +1,9 @@
 package info.izumin.android.rxblescanner;
 
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.ScanResult;
+import android.os.Build;
 
 /**
  * Created by izumin on 1/3/2016 AD.
@@ -14,6 +17,17 @@ public class ScanResultJB {
         this.device = device;
         this.rssi = rssi;
         this.scanRecord = scanRecord;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ScanResultJB(ScanResult result) {
+        this.device = result.getDevice();
+        this.rssi = result.getRssi();
+        if (result.getScanRecord() == null) {
+            this.scanRecord = new byte[]{};
+        } else {
+            this.scanRecord = result.getScanRecord().getBytes();
+        }
     }
 
     public BluetoothDevice getDevice() {
