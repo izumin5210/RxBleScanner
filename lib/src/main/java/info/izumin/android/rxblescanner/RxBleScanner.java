9e6ee5c97507103ee0751b7113c394f8e5bfa5a1
izumin5210 <masayuki@izumin.info>
2016-01-03 23:53:14 +0900

    Implement RxBleScanner

--- lib/src/main/java/info/izumin/android/rxblescanner/RxBleScanner.java
+++ lib/src/main/java/info/izumin/android/rxblescanner/RxBleScanner.java
@@ -1,12 +1,41 @@
 package info.izumin.android.rxblescanner;
 
-import android.content.Context;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.ScanResult;
+import android.os.Build;
+
+import rx.Observable;
 
 /**
  * Created by izumin on 1/2/2016 AD.
  */
 public class RxBleScanner {
 
-    public RxBleScanner(Context context) {
+    private final BluetoothAdapter adapter;
+
+    private RxBleScannerImpl scannerImpl;
+
+    public RxBleScanner(BluetoothAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public Observable<ScanResult> startScan() {
+        final RxBleScannerL scanner = new RxBleScannerL(adapter);
+        scannerImpl = scanner;
+        return scanner.startScan();
+    }
+
+    public Observable<ScanResultJB> startScanJB() {
+        final RxBleScannerJB scanner = new RxBleScannerJB(adapter);
+        scannerImpl = scanner;
+        return scanner.startScan();
+    }
+
+    public void stopScan() {
+        if (scannerImpl != null) {
+            scannerImpl.stopScan();
+        }
     }
 }
